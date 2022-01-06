0d0b0d65154a6474e0d9b0a85399d2d8819bb719
Héber Lima Silva <heberlimasilva@gmail.com>
2022-01-06 17:02:34 -0300

    Usando função map para varrer .json

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 import jeremyAvatar from '../../assets/image-jeremy.png'
 import pointersIcon from '../../assets/icon-ellipsis.svg'
-import Data from '../../data.json'
-import './styles/styles.scss'
 
+import Data from '../../data.json'
+
+import './styles/styles.scss'
 
 export function Dashboard(){
 
@@ -14,7 +15,6 @@ export function Dashboard(){
     function handleClickMenuOptions(event: String){
        setTime(event)     
     }
-
 
     return(
         <div className="container">
@@ -45,159 +45,36 @@ export function Dashboard(){
                     </button>
                 </div>
             </div>
+            {
+                Data.map(cards =>(
+                    <div key={cards.title} className={`card ${cards.title.toString().toLowerCase().replace(/\s+/g, '-')}`}>
+                        <div className="card-description">
+                            <div>
+                                <div className="card-name">{cards.title}</div>
+                                <div className="pointers"><img src={pointersIcon} alt="" /></div>
+                            </div>
+                            <div>
+                                <div className="hours">
+                                    {  
+                                        time === 'daily' ? (cards.timeframes.daily.current + 'hrs') : 
+                                        time === 'weekly' ? (cards.timeframes.weekly.current + 'hrs') :
+                                        time === 'montly' ? (cards.timeframes.monthly.current + 'hrs') : ''
+                                    }
+                                </div>
+                                <div className="description">
+                                    {
+                                    time === 'daily' ? 'Last Day - ' + (cards.timeframes.daily.previous + 'hrs') : 
+                                    time === 'weekly' ? 'Last Week - ' + (cards.timeframes.weekly.previous + 'hrs') :
+                                    time === 'montly' ? 'Last Month - ' + (cards.timeframes.monthly.previous + 'hrs') : ''  
+                                    }
+                                    
+                                </div>
+                            </div>
 
-            <div className="card work">
-                <div className="card-description">
-                    <div>
-                        <div className="card-name">Work</div>
-                        <div className="pointers"><img src={pointersIcon} alt="" /></div>
-                    </div>
-                    <div>
-                        <div className="hours">
-                            {  
-                                time === 'daily' ? (Data[0].timeframes.daily.current + 'hrs') : 
-                                time === 'weekly' ? (Data[0].timeframes.weekly.current + 'hrs') :
-                                time === 'montly' ? (Data[0].timeframes.monthly.current + 'hrs') : ''
-                            }
-                        </div>
-                        <div className="description">
-                            {
-                              time === 'daily' ? 'Last Day - ' + (Data[0].timeframes.daily.previous + 'hrs') : 
-                              time === 'weekly' ? 'Last Week - ' + (Data[0].timeframes.weekly.previous + 'hrs') :
-                              time === 'montly' ? 'Last Month - ' + (Data[0].timeframes.monthly.current + 'hrs') : ''  
-                            }
-                            
                         </div>
                     </div>
-
-                </div>
-            </div>
-            <div className="card play">
-                <div className="card-description">
-                    <div>
-                        <div className="card-name">Play</div>
-                        <div className="pointers"><img src={pointersIcon} alt="" /></div>
-                    </div>
-                    <div>
-                        <div className="hours">
-                            {  
-                                time === 'daily' ? (Data[1].timeframes.daily.current + 'hrs') : 
-                                time === 'weekly' ? (Data[1].timeframes.weekly.current + 'hrs') :
-                                time === 'montly' ? (Data[1].timeframes.monthly.current + 'hrs') : ''
-                            }
-                        </div>
-                        <div className="description">
-                            {
-                              time === 'daily' ? 'Last Day - ' + (Data[1].timeframes.daily.previous + 'hrs') : 
-                              time === 'weekly' ? 'Last Week - ' + (Data[1].timeframes.weekly.previous + 'hrs') :
-                              time === 'montly' ? 'Last Month - ' + (Data[1].timeframes.monthly.current + 'hrs') : ''  
-                            }
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div className="card study">
-                <div className="card-description">
-                    <div>
-                        <div className="card-name">Study</div>
-                        <div className="pointers"><img src={pointersIcon} alt="" /></div>
-                    </div>
-                    <div>
-                        <div className="hours">
-                            {  
-                                time === 'daily' ? (Data[2].timeframes.daily.current + 'hrs') : 
-                                time === 'weekly' ? (Data[2].timeframes.weekly.current + 'hrs') :
-                                time === 'montly' ? (Data[2].timeframes.monthly.current + 'hrs') : ''
-                            }
-                        </div>
-                        <div className="description">
-                            {
-                              time === 'daily' ? 'Last Day - ' + (Data[2].timeframes.daily.previous + 'hrs') : 
-                              time === 'weekly' ? 'Last Week - ' + (Data[2].timeframes.weekly.previous + 'hrs') :
-                              time === 'montly' ? 'Last Month - ' + (Data[2].timeframes.monthly.current + 'hrs') : ''  
-                            }
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div className="card exercise">
-                <div className="card-description">
-                    <div>
-                        <div className="card-name">Exercise</div>
-                        <div className="pointers"><img src={pointersIcon} alt="" /></div>
-                    </div>
-                    <div>
-                        <div className="hours">
-                            {  
-                                time === 'daily' ? (Data[3].timeframes.daily.current + 'hrs') : 
-                                time === 'weekly' ? (Data[3].timeframes.weekly.current + 'hrs') :
-                                time === 'montly' ? (Data[3].timeframes.monthly.current + 'hrs') : ''
-                            }
-                        </div>
-                        <div className="description">
-                            {
-                              time === 'daily' ? 'Last Day - ' + (Data[3].timeframes.daily.previous + 'hrs') : 
-                              time === 'weekly' ? 'Last Week - ' + (Data[3].timeframes.weekly.previous + 'hrs') :
-                              time === 'montly' ? 'Last Month - ' + (Data[3].timeframes.monthly.current + 'hrs') : ''  
-                            }
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div className="card social">
-                <div className="card-description">
-                    <div>
-                        <div className="card-name">Social</div>
-                        <div className="pointers"><img src={pointersIcon} alt="" /></div>
-                    </div>
-                    <div>
-                        <div className="hours">
-                            {  
-                                time === 'daily' ? (Data[4].timeframes.daily.current + 'hrs') : 
-                                time === 'weekly' ? (Data[4].timeframes.weekly.current + 'hrs') :
-                                time === 'montly' ? (Data[4].timeframes.monthly.current + 'hrs') : ''
-                            }
-                        </div>
-                        <div className="description">
-                            {
-                              time === 'daily' ? 'Last Day - ' + (Data[4].timeframes.daily.previous + 'hrs') : 
-                              time === 'weekly' ? 'Last Week - ' + (Data[4].timeframes.weekly.previous + 'hrs') :
-                              time === 'montly' ? 'Last Month - ' + (Data[4].timeframes.monthly.current + 'hrs') : ''  
-                            }
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div className="card self-care">
-                <div className="card-description">
-                    <div>
-                        <div className="card-name">Self Care</div>
-                        <div className="pointers"><img src={pointersIcon} alt="" /></div>
-                    </div>
-                    <div>
-                        <div className="hours">
-                            {  
-                                time === 'daily' ? (Data[5].timeframes.daily.current + 'hrs') : 
-                                time === 'weekly' ? (Data[5].timeframes.weekly.current + 'hrs') :
-                                time === 'montly' ? (Data[5].timeframes.monthly.current + 'hrs') : ''
-                            }
-                        </div>
-                        <div className="description">
-                            {
-                              time === 'daily' ? 'Last Day - ' + (Data[5].timeframes.daily.previous + 'hrs') : 
-                              time === 'weekly' ? 'Last Week - ' + (Data[5].timeframes.weekly.previous + 'hrs') :
-                              time === 'montly' ? 'Last Month - ' + (Data[5].timeframes.monthly.current + 'hrs') : ''  
-                            }
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
+                ))
+            }
         </div>
     )
 }
